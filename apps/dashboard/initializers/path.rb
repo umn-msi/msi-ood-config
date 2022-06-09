8582023a4040119181f21ca06ef9561efbc49547
Matt Mix <mattmix@umn.edu>
2022-06-09 17:14:34 -0500

    Add paths to the files drop down

---
+++ apps/dashboard/initializers/path.rb
@@ -0,0 +1,7 @@
+OodFilesApp.candidate_favorite_paths.tap do |paths|
+
+    add_paths = ["/scratch.global", "/scratch.global/#{User.new.name}"]
+    paths.concat add_paths.map { |p| FavoritePath.new(p) }
+  
+  end
+  
