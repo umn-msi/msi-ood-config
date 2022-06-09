c9ba0c166eaf9791346f536c4ad76a7c6054c1fc
mattmix <mattmix@umn.edu>
2022-06-09 18:46:44 -0500

    Add all group directories too

--- apps/dashboard/initializers/path.rb
+++ apps/dashboard/initializers/path.rb
@@ -1,7 +1,8 @@
 OodFilesApp.candidate_favorite_paths.tap do |paths|
 
-    add_paths = ["/scratch.global", "/scratch.global/#{User.new.name}"]
+    add_paths = User.new.groups.map { |group| "/home/#{group}" }
+    add_paths.concat ["/scratch.global", "/scratch.global/#{User.new.name}"]
+
     paths.concat add_paths.map { |p| FavoritePath.new(p) }
-  
-  end
-  
+
+end
