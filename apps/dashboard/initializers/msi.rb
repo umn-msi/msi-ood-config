befe68eb06d63519ef82fc42f8c6d2dcebad7760
Matt Mix <mattmix@umn.edu>
2022-07-12 11:40:20 -0500

    Add quick resources / partitions

--- apps/dashboard/initializers/msi.rb
+++ apps/dashboard/initializers/msi.rb
@@ -45,5 +45,47 @@ class MSI
     return maint
   end
 
+  quick_resources = {
+    # Format is partition:nodes:ntasks-per-node:memory:tmp:gpus
+    common: [
+      
+    ],
+    mesabi: [
+      ['Interactive - 3 cores, 8 GB, 48 GB local scratch', 'interactive:1:3:8:48:0'],
+      ['Big Mem - 12 cores, 128 GB, 180 GB local scratch', 'bigmem:1:12:128:180:0'],
+      ['K40 GPU - 12 cores, 60 GB, 100 GB local scratch, 1 K40', 'k40:12:60:100:1'],
+    ],
+    agate: [
+      ['Interactive - 2 cores, 32 GB, 64 GB local scratch', 'interactive:1:2:32:64'],
+      ['Big Mem - 32 cores, 500 GB, 190 GB local scratch', 'ag2tb:1:32:500:190'],
+    ],
+  }
+  
+  partitions = { 
+    common: [
+      ['interactive', 'interactive'],
+      ['interactive-gpu', 'interactive-gpu'],
+      ['preempt', 'preempt'],
+      ['preempt-gpu', 'preempt-gpu'],
+      ['interactive-long', 'interactive-long'],
+    ],
+    mesabi: [
+      ['small', 'small'],
+      ['large', 'large'],
+      ['amdsmall', 'amdsmall'],
+      ['amdlarge', 'amdlarge'],
+      ['amd512', 'amd512'],
+      ['amd2tb', 'amd2tb'],
+      ['v100', 'v100'],
+    ],
+    agate: [
+      ['agsmall', 'agsmall'],
+      ['aglarge', 'aglarge'],
+      ['ag2tb', 'ag2tb'],
+      ['a100-4', 'a100-4'],
+      ['a100-8', 'a100-8'],
+    ],
+  }
+
 
 end
