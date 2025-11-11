212f4906d0a011977044c69c4dc057c5bf1d2f22
Matt Mix <mattmix@umn.edu>
2025-11-11 16:21:54 -0600

    Sort paths

--- apps/dashboard/initializers/path.rb
+++ apps/dashboard/initializers/path.rb
@@ -2,10 +2,13 @@ Rails.application.config.after_initialize do
   OodFilesApp.candidate_favorite_paths.tap do |paths|
 
     add_paths = ["/scratch.global", "/scratch.global/#{User.new.name}"]
-    add_paths.concat User.new.groups.map { |group| "/home/#{group}" }
-    add_paths.concat User.new.groups.map { |group| "/projects/standard/#{group}" }
-    add_paths.concat User.new.groups.map { |group| "/projects/regulated/#{group}" }
-
+    
+    User.new.groups.sort.each { |group|
+      add_paths << "/home/#{group}"
+      add_paths << "/projects/standard/#{group}"
+      add_paths << "/projects/regulated/#{group}"
+    }
+    
     paths.concat add_paths.map { |p| FavoritePath.new(p) }
   end
 end
